96b3f1eee57263276f2d01ac5ce3c83d375c0c77
levanphatdat <levanphatdat9@gmail.com>
2023-03-07 09:40:32 +0700

    baitap

--- ss7_spring_data_repository/bai_tap/blog/src/main/java/com/example/blog/service/ICategoryService.java
+++ ss7_spring_data_repository/bai_tap/blog/src/main/java/com/example/blog/service/ICategoryService.java
@@ -1,9 +1,10 @@
 package com.example.blog.service;
 
 import com.example.blog.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface ICategoryService {
     List<Category> findAllCategory();
 
